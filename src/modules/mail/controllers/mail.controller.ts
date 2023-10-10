9cd3efe8c1b16f93f4eca0efbda8312fe33244e0
GanulenkoAF <ganulenkoaf@gmail.com>
2023-10-11 00:12:00 +0300

    fix

--- src/modules/mail/controllers/mail.controller.ts
+++ src/modules/mail/controllers/mail.controller.ts
@@ -1,8 +1,9 @@
-import {Body, Controller, Get, HttpException, HttpStatus, Post} from "@nestjs/common";
+import {Body, Controller, Get, HttpCode, HttpException, HttpStatus, Post, Res} from "@nestjs/common";
 import {IMessagePayload} from "../interfaces/message.interface";
 import {MailService} from "../services/mail.service";
 import {CaptchaService} from "../../captcha/services/captcha.service";
-import {of, switchMap, throwError} from "rxjs";
+import {of, switchMap} from "rxjs";
+import {Response} from "express";
 
 @Controller('api/v1')
 export class MailController {
@@ -15,10 +16,11 @@ export class MailController {
     }
 
     @Post( 'message')
-    postMessage(@Body() body: IMessagePayload) {
+    @HttpCode(200)
+    postMessage(@Body() body: IMessagePayload, @Res({passthrough: true}) res: Response) {
         return this.captchaService.check(body.token)
             .pipe(switchMap(result => result ?
-                this.mailService.sendMessage(body) :
+                this.mailService.sendMessage(body) : res.status(403) &&
                 of(new HttpException('Forbidden', HttpStatus.FORBIDDEN))));
     }
 }
